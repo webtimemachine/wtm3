68c149f724e7e8bfb490c633eabb545fb7304b7b
alex newman <posix4e@gmail.com>
2026-06-30 14:26:50 -0400

    Add Firefox Android extension package

--- extension-chrome/src/background.ts
+++ extension-chrome/src/background.ts
@@ -99,6 +99,7 @@ function deviceName(): string {
   if (PLATFORM === "safari-ios") {
     return /iPad/.test(ua) ? "Safari on iPad" : "Safari on iPhone";
   }
+  if (PLATFORM === "firefox-android") return "Firefox on Android";
   const os = /Macintosh|Mac OS/.test(ua)
     ? "macOS"
     : /Windows/.test(ua)
